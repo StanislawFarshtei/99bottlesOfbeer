a099087c352327fb9e2aa8f7d2738398b8bbfce8
StanislawFarshtei <64971911+StanislawFarshtei@users.noreply.github.com>
2021-10-25 15:32:12 +0300

    while loop

--- JavaScript exercises/99bottlesOfMilk.js
+++ JavaScript exercises/99bottlesOfMilk.js
@@ -2,14 +2,11 @@
 let count = 99;
 
 function beer() {
-    if (count === 1) {
 
-    }
     while (count >= 1) {
         count--;
-        console.log(`99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.
-
-`);
+        console.log(`${count} bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.`);
     }
+
 }
 beer();
