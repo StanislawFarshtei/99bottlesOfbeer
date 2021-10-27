a23562cdc52eb91ef2081108b076a7c8259a5f91
StanislawFarshtei <stanislaw.farshtei@gmail.com>
2021-10-27 12:36:49 +0300

    Random Dice number Game - Dicee! Learning to work with DOM in plane JS.

---
+++ DOM/index.js
@@ -0,0 +1,7 @@
+// document.querySelector("ul").lastElementChild.innerHTML = "Stas";
+// document.getElementsByClassName("btn")
+// document.getElementById("list").innerHTML = "Good Bye";
+// document.querySelector("#title");
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").textContent = "red";
+
